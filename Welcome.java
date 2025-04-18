057cda695efb80a85ac34fd093883f365fe21fd0
Geetha <danda.saigeetha@wipro.com>
2025-04-18 13:09:03 +0000

    Welcome.java file commit3

--- Welcome.java
+++ Welcome.java
@@ -1,11 +1,7 @@
-
 public class Welcome
-
 {
-
-public static void main (String args[])
-
-{
-System.Out.println("Welcome to Jenkins CICD");
-}
+    public static void main(String[] args)
+    {
+        System.out.println(“Hello, World”);
+    }
 }
